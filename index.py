a71e831f80e5777a3ad091e1f911b6cff93c4c6c
EJ <ortegaenriquejose@gmail.com>
2019-09-13 12:09:53 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,90 @@
+# /index.py
+from flask import Flask, request, jsonify, render_template
+import os
+import dialogflow
+import requests
+import json
+
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json(silent=True)
+    action = get_action(data);
+
+    if action == 'buy_tshirt_action':
+        reply = buy_tshirt_action(data)
+        return jsonify(reply)
+    elif action == 'buy-tshirt.buy-tshirt-no':
+        reply = buy_tshirt_no_action(data)
+        return jsonify(reply)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form['message']
+    project_id = os.getenv('DIALOGFLOW_PROJECT_ID')
+    fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
+    response_text = { "message":  fulfillment_text }
+    return jsonify(response_text)
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    if text:
+        text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        return response.query_result.fulfillment_text
+
+def get_action(data):
+    req = request.get_json(force=True)
+    return req.get('queryResult').get('action')
+
+def get_parameters(data):
+    return data['queryResult']['outputContexts'][0]['parameters']
+
+def buy_tshirt_action(data):
+    tshirt_size = get_parameters(data)['tshirt_size']
+    color = get_parameters(data)['color']
+    price = 200.25
+
+    if tshirt_size == 'small':
+        price += 150.50
+    elif tshirt_size == 'medium':
+        price += 220.20
+    elif tshirt_size == 'large':
+        price += 299.75
+
+    price = price * 2.75;    
+    reply = {
+        "fulfillmentText": "Great! You ordered color {} and {} size tshirt! The total price is {}. Come pick it up tomorrow in our branch at 1230 Makati City, Metro Manila. Would you like a receipt?".format(color, tshirt_size, price)
+    }
+
+    return reply
+
+def buy_tshirt_no_action(data):
+    order_id = randint(1000, 10000)
+    reply = {
+        "fulfillmentText": "Cool no problem. Your Order Id is {}. Thank you for purchasing an item in Basic Tshirt!".format(order_id)
+    }
+
+    return reply
+
+# run Flask app
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,52 @@
+function submit_message(message) {
+    $.post( "/send_message", {message: message}, handle_response);
+
+    function handle_response(data) {
+      // append the bot repsonse to the div
+      $('.chat-container').append(`
+            <div class="chat-message bot-message">
+                <div class="chat-image">
+                    <img src="https://imgur.com/3pgjFvq.png" />
+                </div>
+                <b>Bot: </b>
+                ${data.message}
+            </div>
+      `)
+      // remove the loading indicator
+      $( "#loading" ).remove();
+    }
+    // auto scroll
+    $(".chat-container").stop().animate({ scrollTop: $(".chat-container")[0].scrollHeight}, 1000);
+}
+
+$('#target').on('submit', function(e){
+    e.preventDefault();
+    const input_message = $('#input_message').val()
+    // return if the user does not enter any text
+    if (!input_message) {
+      return
+    }
+
+    $('.chat-container').append(`
+        <div class="chat-message human-message">
+            <div class="chat-image">
+                <img src="https://imgur.com/Fcos3Vt.png" />
+            </div>
+            <b>You: </b>
+            ${input_message}
+        </div>
+    `)
+
+    // loading 
+    $('.chat-container').append(`
+        <div class="chat-message bot-message" id="loading">
+            <b>...</b>
+        </div>
+    `)
+
+    // clear the text input 
+    $('#input_message').val('')
+
+    // send the message
+    submit_message(input_message)
+});
